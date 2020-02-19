13b52a3ae09e4e6e98e837e0f3559ac3bfc8c82a
zment <jani.karkkainen@gmail.com>
2020-02-19 06:59:59 +0200

    Mostly working

---
+++ HacknPlanExtension.cs
@@ -0,0 +1,255 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Linq;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Windows.Forms;
+
+namespace Codice.Client.IssueTracker.HacknPlan
+{
+    public class HacknPlanExtension : IPlasticIssueTrackerExtension
+    {
+        public static readonly string USERNAME_KEY = "Username";
+        public static readonly string BRANCH_PREFIX_KEY = "Branch Prefix";
+        public static readonly string API_SECRET_KEY = "API Secret";
+        public static readonly string PROJECT_ID_KEY = "Project Id";
+
+        IssueTrackerConfiguration _config;
+
+        static readonly ILog _log = LogManager.GetLogger("HacknPlan");
+
+        HttpClient httpClient = new HttpClient();
+
+        private string _projectId;
+        private int _userId;
+        private string _userName;
+        private bool _connected;
+        public bool Connected {
+            get {
+                if (!_connected)
+                {
+                    _log.Error("Calling interface methods while not connected");
+                    return _connected;
+                }
+
+                return _connected;
+            }
+        }
+        internal HacknPlanExtension(IssueTrackerConfiguration config)
+        {
+            _config = config;
+            _log.Info("HacknPlan issue tracker extension initialized");
+            _projectId = config.GetValue(PROJECT_ID_KEY);
+        }
+
+        public void Connect()
+        {
+            httpClient = GetHttpClient(_config);
+
+            try
+            {
+                var meJson = httpClient.GetStringAsync("users/me").Result;
+                var me = JsonConvert.DeserializeObject<dynamic>(meJson);
+                _userId = me.id;
+                _userName = me.username;
+                _connected = true;
+            } catch (AggregateException ae)
+            {
+                ae.Handle((x) =>
+                {
+                    if (x is HttpRequestException)
+                    {
+                        _log.Error(x.Message);
+
+                        _connected = false;
+
+                        return true;
+                    }
+
+                    _log.Error(ae.Message);
+
+                    return false;
+                });
+            }
+        }
+
+        public void Disconnect()
+        {
+            httpClient.Dispose();
+        }
+
+        public string GetExtensionName()
+        {
+            return "HacknPlan";
+        }
+
+        public List<PlasticTask> GetPendingTasks()
+        {
+            if (!Connected) return new List<PlasticTask>();
+
+            return GetPendingTasksInternal();
+        }
+
+        public List<PlasticTask> GetPendingTasks(string assignee)
+        {
+            if (!Connected) return new List<PlasticTask>();
+
+            return GetPendingTasksInternal(assignee);
+        }
+
+        public List<PlasticTask> GetPendingTasksInternal(string assignee = "")
+        {
+            var userIdQueryItem = string.IsNullOrEmpty(assignee) ? "" : $"&userId={_userId}";
+            var query = $"projects/{_projectId}/workitems?limit=100&stageId=5{userIdQueryItem}";
+            _log.Info(query);
+
+            var responseJson = httpClient.GetStringAsync(query).Result;
+            _log.Info($"{responseJson}");
+            var data = JsonConvert.DeserializeObject<dynamic>(responseJson);
+
+            var taskList = new List<PlasticTask>();
+
+            foreach (var workItem in data.items)
+            {
+                if (workItem.board.boardId == 287859) 
+                    continue;
+
+                taskList.Add(new PlasticTask() {
+                    CanBeLinked = true,
+                    Id = workItem.workItemId,
+                    Title = workItem.title,
+                    Description = workItem.description,
+                    Owner = workItem.user.username,
+                    Status = workItem.stage.status
+                });
+            }
+
+            return taskList;
+        }
+
+        public PlasticTask GetTaskForBranch(string fullBranchName)
+        {
+            if (!Connected) return default;
+
+            var id = GetTaskIdFromBranchName(fullBranchName);
+            if (string.IsNullOrEmpty(id)) return default;
+
+            var workItem = GetWorkItemAsJson(id);
+            if (workItem == null) return default;
+
+            return new PlasticTask() {
+                CanBeLinked = true,
+                Id = id,
+                Title = workItem.title,
+                Description = workItem.description,
+                Owner = workItem.user.username,
+                Status = workItem.stage.status
+            };
+        }
+
+        private dynamic GetWorkItemAsJson(string id)
+        {
+            string body = "";
+            try
+            {
+                body = httpClient.GetStringAsync($"projects/{_projectId}/workitems/{id}").Result;
+            }
+            catch (AggregateException e)
+            {
+                e.Handle((x) =>
+                {
+                    if (x is HttpRequestException)
+                    {
+                        _log.Error($"Task API request failed: {x.Message}");
+                        return true;
+                    }
+
+                    return false;
+                });
+
+                return default;
+            }
+
+            return JsonConvert.DeserializeObject<dynamic>(body);
+        }
+
+        private string GetTaskIdFromBranchName(string fullBranchName)
+        {
+            try
+            {
+                return fullBranchName.Split('_').Last().Split('/').First();
+            }
+            catch (NullReferenceException)
+            {
+                _log.Error($"Could not parse task id from {fullBranchName}");
+            }
+
+            return null;
+        }
+
+        public Dictionary<string, PlasticTask> GetTasksForBranches(List<string> fullBranchNames)
+        {
+            return new Dictionary<string, PlasticTask>();
+        }
+
+        public List<PlasticTask> LoadTasks(List<string> taskIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogCheckinResult(PlasticChangeset changeset, List<PlasticTask> tasks)
+        {
+        }
+
+        public void MarkTaskAsOpen(string taskId, string assignee)
+        {
+        }
+
+        public void OpenTaskExternally(string taskId)
+        {
+            Process.Start($"https://app.hacknplan.com/p/72274/kanban?taskId={taskId}");
+        }
+
+        public bool TestConnection(IssueTrackerConfiguration configuration)
+        {
+            var httpClient = GetHttpClient(configuration);
+
+            try
+            {
+                var meJson = httpClient.GetStringAsync("users/me").Result;
+                var me = JsonConvert.DeserializeObject<dynamic>(meJson);
+                MessageBox.Show($"Username: {me.username}\nUser Id: {me.id}", "Connection Succesful", MessageBoxButtons.OK);
+                return true;
+            }
+            catch (Exception e)
+            {
+                var msg = e.Message;
+                if (e is AggregateException)
+                    msg = e.InnerException.Message;
+
+                MessageBox.Show(msg, "Connection Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return false;
+            }
+
+        }
+
+        private HttpClient GetHttpClient(IssueTrackerConfiguration configuration)
+        {
+            var httpClient = new HttpClient();
+
+            httpClient.BaseAddress = new Uri($@"https://api.hacknplan.com/v0/");
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("ApiKey", configuration.GetValue(API_SECRET_KEY));
+            httpClient.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue() { NoCache = true };
+
+            return httpClient;
+        }
+
+        public void UpdateLinkedTasksToChangeset(PlasticChangeset changeset, List<string> tasks)
+        {
+        }
+    }
+}
---
+++ HacknPlanExtensionFactory.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Codice.Client.IssueTracker;
+
+namespace Codice.Client.IssueTracker.HacknPlan
+{
+    class HacknPlanExtensionFactory : IPlasticIssueTrackerExtensionFactory
+    {
+
+
+        public IssueTrackerConfiguration GetConfiguration(IssueTrackerConfiguration storedConfiguration)
+        {
+            var workingMode = GetWorkingMode(storedConfiguration);
+
+            var user = GetValidParameterValue(storedConfiguration, HacknPlanExtension.USERNAME_KEY, "");
+            var prefix = GetValidParameterValue(storedConfiguration, HacknPlanExtension.BRANCH_PREFIX_KEY, "task_");
+            var apiSecret = GetValidParameterValue(storedConfiguration, HacknPlanExtension.API_SECRET_KEY, "");
+            var projectId = GetValidParameterValue(storedConfiguration, HacknPlanExtension.PROJECT_ID_KEY, "");
+
+            var userIdParam = new IssueTrackerConfigurationParameter()
+            {
+                Name = HacknPlanExtension.USERNAME_KEY,
+                Value = user,
+                Type = IssueTrackerConfigurationParameterType.User,
+                IsGlobal = false
+            };
+
+            var branchPrefixParam = new IssueTrackerConfigurationParameter()
+            {
+                Name = HacknPlanExtension.BRANCH_PREFIX_KEY,
+                Value = prefix,
+                Type = IssueTrackerConfigurationParameterType.BranchPrefix,
+                IsGlobal = true
+            };
+
+            var apiSecretParam = new IssueTrackerConfigurationParameter()
+            {
+                Name = HacknPlanExtension.API_SECRET_KEY,
+                Value = apiSecret,
+                Type = IssueTrackerConfigurationParameterType.Text,
+                IsGlobal = true
+            };
+
+            var projectIdParam = new IssueTrackerConfigurationParameter()
+            {
+                Name = HacknPlanExtension.PROJECT_ID_KEY,
+                Value = projectId,
+                Type = IssueTrackerConfigurationParameterType.Text,
+                IsGlobal = true
+            };
+
+            var parameters = new List<IssueTrackerConfigurationParameter>()
+            {
+                branchPrefixParam,
+                apiSecretParam,
+                projectIdParam
+            };
+
+            return new IssueTrackerConfiguration(workingMode, parameters);
+        }
+
+        private string GetValidParameterValue(IssueTrackerConfiguration config, string paramName, string defaultValue)
+        {
+            string configValue = config?.GetValue(paramName);
+
+            if (string.IsNullOrEmpty(configValue))
+                return defaultValue;
+
+            return configValue;
+        }
+
+        private ExtensionWorkingMode GetWorkingMode(IssueTrackerConfiguration config)
+        {
+            if (config == null)
+                return ExtensionWorkingMode.TaskOnBranch;
+
+            if (config.WorkingMode == ExtensionWorkingMode.None)
+                return ExtensionWorkingMode.TaskOnBranch;
+
+            return config.WorkingMode;
+        }
+
+        public IPlasticIssueTrackerExtension GetIssueTrackerExtension(IssueTrackerConfiguration configuration)
+        {
+            return new HacknPlanExtension(configuration);
+        }
+
+        public string GetIssueTrackerName()
+        {
+            return "HacknPlan Issue Tracker";
+        }
+    }
+}
